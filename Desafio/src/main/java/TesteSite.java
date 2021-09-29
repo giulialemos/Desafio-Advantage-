05dc46ffdab8ceb074e0f7fd6d0bdd80f0c3a070
giulialemos <giulia.lemos@keeggo.com>
2021-09-28 22:12:23 -0300

    Second Commit

--- Desafio/src/main/java/TesteSite.java
+++ Desafio/src/main/java/TesteSite.java
@@ -1,16 +1,40 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 
 public class TesteSite {
 
 	@Test
-	public void test() {
-		// WebDriver driver = new FirefoxDriver();
-		WebDriver driver = new ChromeDriver();
+	public void teste() {
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("http://advantageonlineshopping.com/#/");
 		driver.quit();
+	}
+	
+	@Test
+	public void cadastro() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://advantageonlineshopping.com/#/");
+		driver.findElement(By.id("menuUser")).click();
+		driver.findElement(By.id("menuUser")).click();
+		driver.findElement(By.linkText("CREATE NEW ACCOUNT")).click();
+		driver.get("http://advantageonlineshopping.com/#/register");
+	}
+	
+	@Test
+	public void consultarProduto() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://advantageonlineshopping.com/#/");
+		driver.findElement(By.id("menuSearch")).click();
+		driver.findElement(By.id("autoComplete")).sendKeys("headphones");
+		driver.findElement(By.id("menuSearch")).click();
+		Assert.assertEquals("headphones", driver.findElement(By.id("autoComplete")).getAttribute("value"));
 	}
 }
